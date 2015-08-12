4d3e3a397421b5cad874f208a93616bb3b4f7be1
Joao Neves <joaoneves792@gmail.com>
2015-08-12 22:18:01 +0100

    Rewrite of the loadPNG function and corrected bug that prevented alpha in transparent textures

--- Textures.cpp
+++ Textures.cpp
@@ -27,66 +27,145 @@ typedef struct {
     unsigned char *data;
 }textureImage;
 
-int loadPNG(const char* filename, textureImage* texture){
-	png_structp png_ptr;
-	png_infop info_ptr;
-	unsigned int sig_read = 0;
-	int color_type, interlace_type;
-	FILE *fp;
+int loadPNG(const char* file_name, textureImage* texture){
+    // This function was originally written by David Grayson for
+    // https://github.com/DavidEGrayson/ahrs-visualizer
+    //Adapted by Joao Neves
+    //on 12-8-2015
 
-	if ((fp = fopen(filename, "rb")) == NULL)
+    png_byte header[8];
+
+    FILE *fp = fopen(file_name, "rb");
+    if (fp == 0){
+        perror(file_name);
+        return 0;
+    }
+    // read the header
+    fread(header, 1, 8, fp);
+    if (png_sig_cmp(header, 0, 8)){
+        fprintf(stderr, "error: %s is not a PNG.\n", file_name);
+        fclose(fp);
+        return 0;
+    }
+
+    png_structp png_ptr = png_create_read_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
+    if (!png_ptr){
+        fprintf(stderr, "error: png_create_read_struct returned 0.\n");
+        fclose(fp);
+        return 0;
+    }
+
+    // create png info struct
+    png_infop info_ptr = png_create_info_struct(png_ptr);
+    if (!info_ptr){
+        fprintf(stderr, "error: png_create_info_struct returned 0.\n");
+        png_destroy_read_struct(&png_ptr, (png_infopp)NULL, (png_infopp)NULL);
+        fclose(fp);
+        return 0;
+    }
+
+    // create png info struct
+    png_infop end_info = png_create_info_struct(png_ptr);
+    if (!end_info){
+        fprintf(stderr, "error: png_create_info_struct returned 0.\n");
+        png_destroy_read_struct(&png_ptr, &info_ptr, (png_infopp) NULL);
+        fclose(fp);
+        return 0;
+    }
+
+    // the code in this if statement gets called if libpng encounters an error
+    if (setjmp(png_jmpbuf(png_ptr))) {
+        fprintf(stderr, "error from libpng\n");
+        png_destroy_read_struct(&png_ptr, &info_ptr, &end_info);
+        fclose(fp);
+        return 0;
+    }
+
+    // init png reading
+    png_init_io(png_ptr, fp);
+
+    // let libpng know you already read the first 8 bytes
+    png_set_sig_bytes(png_ptr, 8);
+
+    // read all the info up to the image data
+    png_read_info(png_ptr, info_ptr);
+
+    // variables to pass to get info
+    int bit_depth, color_type;
+    png_uint_32 temp_width, temp_height;
+
+    // get info about png
+    png_get_IHDR(png_ptr, info_ptr, &temp_width, &temp_height, &bit_depth, &color_type, NULL, NULL, NULL);
+
+    
+    texture->width = temp_width;
+    texture->height = temp_height;
+    
+    if (bit_depth != 8){
+        fprintf(stderr, "%s: Unsupported bit depth %d.  Must be 8.\n", file_name, bit_depth);
+        return 0;
+    }
+
+    GLint format;
+    switch(color_type){
+	case PNG_COLOR_TYPE_RGB:
+        	format = GL_RGB;
+       		texture->alpha = false;
+		break;
+    	case PNG_COLOR_TYPE_RGB_ALPHA:
+        	format = GL_RGBA;
+		texture->alpha = true;
+        	break;
+    	default:
+        	fprintf(stderr, "%s: Unknown libpng color type %d.\n", file_name, color_type);
         	return 0;
-	png_ptr = png_create_read_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
-	if (png_ptr == NULL) {
-		fclose(fp);
-		return 0;
-	}
-	info_ptr = png_create_info_struct(png_ptr);
-	if (info_ptr == NULL) {
-		fclose(fp);
-		png_destroy_read_struct(&png_ptr, NULL, NULL);
-		return 0;
-	}
-	if (setjmp(png_jmpbuf(png_ptr))) {
-		png_destroy_read_struct(&png_ptr, &info_ptr, NULL);
-		fclose(fp);
-		return 0;
-	}
-	png_init_io(png_ptr, fp);
+    }
 
-	png_set_sig_bytes(png_ptr, sig_read);
-	png_read_png(png_ptr, info_ptr, PNG_TRANSFORM_STRIP_16 | PNG_TRANSFORM_PACKING | PNG_TRANSFORM_EXPAND, NULL);
+    // Update the png info struct.
+    png_read_update_info(png_ptr, info_ptr);
+
+    // Row size in bytes.
+    int rowbytes = png_get_rowbytes(png_ptr, info_ptr);
+
+    // glTexImage2d requires rows to be 4-byte aligned
+    rowbytes += 3 - ((rowbytes-1) % 4);
+
+    // Allocate the image_data as a big block, to be given to opengl
+    texture->data = (png_byte *)malloc(rowbytes * temp_height * sizeof(png_byte)+15);
+    if (texture->data == NULL)
+    {
+        fprintf(stderr, "error: could not allocate memory for PNG image data\n");
+        png_destroy_read_struct(&png_ptr, &info_ptr, &end_info);
+        fclose(fp);
+        return 0;
+    }
+
+    // row_pointers is for pointing to image_data for reading the png with libpng
+    png_byte ** row_pointers = (png_byte **)malloc(temp_height * sizeof(png_byte *));
+    if (row_pointers == NULL)
+    {
+        fprintf(stderr, "error: could not allocate memory for PNG row pointers\n");
+        png_destroy_read_struct(&png_ptr, &info_ptr, &end_info);
+        free(texture->data);
+        fclose(fp);
+        return 0;
+    }
+
+    // set the individual row_pointers to point at the correct offsets of image_data
+    for (unsigned int i = 0; i < temp_height; i++)
+    {
+        row_pointers[temp_height - 1 - i] = texture->data + i * rowbytes;
+    }
+
+    // read the png into image_data through row_pointers
+    png_read_image(png_ptr, row_pointers);
 
 
-
-	texture->width = png_get_image_width(png_ptr,  info_ptr);
-	texture->height = png_get_image_height(png_ptr, info_ptr);
-
-	switch (png_get_color_type(png_ptr, info_ptr)) {
-		case PNG_COLOR_TYPE_RGBA:
-			texture->alpha = true;
-			break;
-		case PNG_COLOR_TYPE_RGB:
-			texture->alpha = false;
-			break;
-		default:
-		std::cout << "Color type " << png_get_color_type(png_ptr, info_ptr) << " not supported" << std::endl;
-		png_destroy_read_struct(&png_ptr, &info_ptr, NULL);
-		fclose(fp);
-		return 0;
-	}
-	unsigned int row_bytes = png_get_rowbytes(png_ptr, info_ptr);
-	texture->data = (unsigned char*)malloc(row_bytes * texture->height);
-
-	png_bytepp row_pointers = png_get_rows(png_ptr, info_ptr);
-
-	for (int i = 0; i < texture->height; i++) {// note that png is ordered top to
-		memcpy(texture->data+(row_bytes * (texture->height-1-i)), row_pointers[i], row_bytes);
-	}
-
-	png_destroy_read_struct(&png_ptr, &info_ptr, NULL);
-	fclose(fp);
-	return 1;
+    // clean up
+    png_destroy_read_struct(&png_ptr, &info_ptr, &end_info);
+    free(row_pointers);
+    fclose(fp);
+    return 1;
 }
 
 int loadJPEG(const char* filename, textureImage* texture){
@@ -247,7 +326,7 @@ GLuint LoadGLTexture(const char* name){
         glGenTextures(1, &texPntr[0]);   /* create the texture */
         glBindTexture(GL_TEXTURE_2D, texPntr[0]);
         /* actually generate the texture */
-        glTexImage2D(GL_TEXTURE_2D, 0, 3, texti->width, texti->height, 0,
+        glTexImage2D(GL_TEXTURE_2D, 0, (texti->alpha)?GL_RGBA:GL_RGB, texti->width, texti->height, 0,
             (texti->alpha)?GL_RGBA:GL_RGB, GL_UNSIGNED_BYTE, texti->data);
         /* enable linear filtering */
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
